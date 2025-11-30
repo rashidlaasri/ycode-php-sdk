319889d189e676abe7b43d90d5fbbfb05ad8408a
Rachid Laasri <rashidlaasri@gmail.com>
2025-11-30 22:40:46 +0100

    Add Service Provider

---
+++ src/YCodeServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RashidLaasri\YCODE;
+
+use Illuminate\Support\ServiceProvider;
+
+class YCodeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(YCode::class,
+            fn (): YCode => new YCode(config('ycode.base_url'), config('ycode.token'))
+        );
+    }
+
+    /**
+     * Bootstrap any package services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/ycode.php' => config_path('ycode.php'),
+        ]);
+    }
+}
